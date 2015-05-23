e36c12007dd7c31b4b440d85248a1da24bcef3a2
mattdesl <dave.des@gmail.com>
2015-05-23 14:21:26 -0400

    working on shaders

--- src/glo-shader/index.js
+++ src/glo-shader/index.js
@@ -1,14 +1,14 @@
 var compileShader = require('./compile-shader')
 var linkShaders = require('./link-program')
 var extract = require('gl-shader-extract')
-var reflect = require('./reflect')
+var reflect = require('glsl-extract-reflect')
 
 module.exports = createShader
 function createShader (gl, opt) {
   var attributes = opt.attributes || []
   var quiet = opt.quiet
   var program = gl.createProgram()
-  var vertex, fragment, types
+  var vertexShader, fragmentShader, types, uniforms
 
   var shader = {
     dispose: disposeProgram,
@@ -21,46 +21,37 @@ function createShader (gl, opt) {
 
   // probably could use a wrapper to clean these up
   Object.defineProperties(shader, {
-    handle: {
-      configurable: true, enumerable: true,
-      get: function () {
-        return program
-      }
-    },
-    vertexShader: {
-      configurable: true, enumerable: true,
-      get: function () {
-        return vertex
-      }
-    },
-    fragmentShader: {
-      configurable: true, enumerable: true,
-      get: function () {
-        return fragment
-      }
-    },
-    types: {
-      configurable: true, enumerable: true,
-      get: function () {
-        return types
-      }
-    }
+    handle: getter(function () {
+      return program
+    }),
+    vertexShader: getter(function () {
+      return vertexShader
+    }),
+    fragmentShader: getter(function () {
+      return fragmentShader
+    }),
+    types: getter(function () {
+      return types
+    }),
+    uniforms: getter(function () {
+      return uniforms
+    })
   })
 
   return shader
 
   function bind () {
-    gl.useShader(program)
+    gl.useProgram(program)
   }
 
   function disposeShaders () {
-    if (vertex) {
-      gl.detachShader(program, vertex)
-      gl.deleteShader(vertex)
+    if (vertexShader) {
+      gl.detachShader(program, vertexShader)
+      gl.deleteShader(vertexShader)
     }
-    if (fragment) {
-      gl.detachShader(program, fragment)
-      gl.deleteShader(fragment)
+    if (fragmentShader) {
+      gl.detachShader(program, fragmentShader)
+      gl.deleteShader(fragmentShader)
     }
   }
 
@@ -74,31 +65,80 @@ function createShader (gl, opt) {
     disposeShaders()
 
     // re-compile source
-    vertex = compileShader(gl, gl.VERTEX_SHADER, vertSrc, quiet)
-    fragment = compileShader(gl, gl.FRAGMENT_SHADER, fragSrc, quiet)
+    vertexShader = compileShader(gl, gl.VERTEX_SHADER, vertSrc, quiet)
+    fragmentShader = compileShader(gl, gl.FRAGMENT_SHADER, fragSrc, quiet)
 
     // re-link
-    linkShaders(gl, program, [ vertex, fragment ], attributes, quiet)
+    var shaders = [ vertexShader, fragmentShader ]
+    linkShaders(gl, program, shaders, attributes, quiet)
 
     // extract uniforms and attributes
     types = extract(gl, program)
 
-    // determine new locations
-    var locations = types.uniforms.map(function(uniform) {
-      return {
-        index: uniform,
-        location: gl.getUniformLocation(program, uniform.name)
-      }
+    // provide optimized getters/setters
+    uniforms = reflect(types.uniforms, function (uniform, index) {
+      return makeUniformProp(uniform, index)
     })
+  }
 
-    console.log(locations)
+  function makeUniformProp (uniform, index) {
+    /*eslint-disable no-new-func*/
+    var path = uniform.path
+    var type = uniform.type
+    var location = gl.getUniformLocation(program, path)
+    var setter = getPropSetter(path, location, type)
 
-    // un-nest all structs and arrays
-    // var structure = {
-    //   uniforms: reflect(types.uniforms),
-    //   attributes: reflect(types.attributes)
-    // }
+    var generated = new Function('self', 'gl', 'program', 'location', [
+      'return function (value, transposed) {',
+        'if (typeof value === "undefined")',
+          'return gl.getUniform(program, location)',
+        'else {',
+          setter,
+          'return self',
+        '}',
+      '}'
+    ].join('\n'))
+    return generated(shader, gl, program, location)
+  }
+}
 
-    
+function getter (fn) {
+  return {
+    configurable: true,
+    enumerable: true,
+    get: fn
+  }
+}
+
+function getPropSetter (path, location, type) {
+  // currently this just handles GLSL ES 1.0
+  // https://www.khronos.org/files/opengles_shading_language.pdf
+  switch (type) {
+    case 'bool':
+    case 'int':
+    case 'sampler2D':
+    case 'samplerCube':
+      return 'gl.uniform1i(location, value)'
+    case 'float':
+      return 'gl.uniform1f(location, value)'
+  }
+
+  var vecIdx = type.indexOf('vec')
+  var count = parseInt(type.charAt(type.length - 1), 10)
+  if (vecIdx === 0 || vecIdx === 1) {
+    var vtype = type.charAt('0')
+    switch (vtype) {
+      case 'b':
+      case 'i':
+        return 'gl.uniform' + count + 'iv(location, value)'
+      case 'v': // regular vecN
+        return 'gl.uniform' + count + 'fv(location, value)'
+      default:
+        throw new Error('unrecognized uniform type ' + type + ' for ' + path)
+    }
+  } else if (type.indexOf('mat') === 0 && type.length === 4) {
+    return 'gl.uniformMatrix' + count + 'fv(location, Boolean(transposed), value)'
+  } else {
+    throw new Error('unrecognized uniform type ' + type + ' for ' + path)
   }
 }
--- tests/test-shader-vars.js
+++ tests/test-shader-vars.js
@@ -2,6 +2,7 @@ var test = require('tape')
 
 var createShader = require('../src/glo-shader')
 var createContext = require('webgl-context')
+var mat4 = require('gl-mat4')
 
 var glslify = require('glslify')
 var gl = createContext()
@@ -16,14 +17,29 @@ test('shader should compile', function (t) {
   })
 
   t.deepEqual(result.types, expected, 'provides types')
+  result.bind()
 
-  // t.equal(result.types.uniforms)
-  // var result = require('../src/glo-shader/reflect')(result.types.uniforms, true)
-  // console.log(JSON.stringify(result, undefined, 2))
+  var tint = result.uniforms.tint()
+  t.deepEqual(tint instanceof Float32Array, true, 'vec4 getter')
+  t.deepEqual(tint.length, 4, 'length is 4')
+  t.deepEqual(result.uniforms.bools(), [ false, false ], 'bvec2 getter')
+  t.deepEqual(result.uniforms.tint3(), [ 0, 0, 0 ], 'vec3 getter')
+  t.deepEqual(result.uniforms.tex(), 0, 'sampler2D getter')
 
-  var loc = gl.getUniformLocation(result.handle, 'lights[0].falloff')
-  console.log(loc)
+  var identity = mat4.identity([])
+  var zeroes = identity.map(function () {
+    return 0
+  })
+  t.deepEqual(result.uniforms.model(), zeroes, 'mat4 getter')
 
+  result.uniforms.model(identity)
+  t.deepEqual(result.uniforms.model(), identity, 'setter works')
+  t.deepEqual(result.uniforms.lights.length, 1, 'only 1 light uniform active')
+
+  var loc = gl.getUniformLocation(result.handle, 'lights[0].ambient')
+  result.uniforms.lights[0].ambient([ 1, 0, 0.5 ])
+  t.deepEqual(gl.getUniform(result.handle, loc), [ 1, 0, 0.5 ], 'sets light struct array')
+  t.deepEqual(gl.getUniform(result.handle, loc), [ 1, 0, 0.5 ], 'sets light struct array')
   result.dispose()
   t.end()
 })
